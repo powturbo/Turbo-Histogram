61a58e683f0bda645606b26afcefec99e3b64313
powturbo <powturbo@gmail.com>
2014-10-05 15:55:53 +0200

    Update turbohist.c

--- turbohist.c
+++ turbohist.c
@@ -1,5 +1,4 @@
 // compile w. gcc -O3 -msse4.1 turbohist.c -o turbohist
-// set #define BUFMAX (1<<16) for small buffer test
 // homepage: http://sites.google.com/site/powturbo/ 
 
 #include <stdio.h>
@@ -10,6 +9,10 @@
   #ifdef __SSE4_1__
 #include <smmintrin.h>
   #endif
+
+//#define BUFMAX (1<<30)  
+#define BUFMAX (1<<16) // small buffer size
+
 //------------------------------------------------------------------------------------
 typedef unsigned long long tm_t;
 static tm_t tmtime(void);
@@ -53,15 +56,12 @@ static double tmmsec(tm_t tm) { return (double)tm/1000.0; }
 //#define RET { unsigned a = 256; while(a > 1 && !c0[a-1]) a--; return a;}
 #define RET { unsigned a=0, i; for(i = 0; i < 256; i++) a+=c0[i]; return a;}
 
-//#define BUFMAX (1<<16)
-#define BUFMAX (1<<30)
-
-  #if BUFMAX <= (1<<16) 
-typedef unsigned short bin_t;
-#define PAD8 0
-  #else
+  #if BUFMAX > (1<<16)
 typedef unsigned       bin_t;
 #define PAD8 16  // +16 to avoid repeated call into the same cache line pool (cache associativity). by Nathan Kurz : https://github.com/powturbo/turbohist/issues/2
+  #else
+typedef unsigned short bin_t;
+#define PAD8 0
   #endif
 
 int hist_1_8(unsigned char *in, unsigned inlen) { 
@@ -215,8 +215,7 @@ int hist_8_64(unsigned char *in, unsigned inlen) {
 //-------------------------------------------------------------------------------------------------
   #ifdef __SSE4_1__
 int hist_4_128(unsigned char *in, unsigned inlen) { 
-  bin_t c0[256]={0},c1[256]={0},c2[256]={0},c3[256]={0}; 
-
+    bin_t c0[256]={0},c1[256]={0},c2[256]={0},c3[256]={0}; 
   unsigned char *ip;
   __m128i vcp = _mm_loadu_si128((__m128i*)in);
   for(ip = in; ip != in+(inlen&~(16-1)); ) {
@@ -238,8 +237,7 @@ int hist_4_128(unsigned char *in, unsigned inlen) {
     c2[_mm_extract_epi8(vc, 14)]++;
     c3[_mm_extract_epi8(vc, 15)]++;
   }
-  while(ip < in+inlen) 
-    c0[*ip++]++; 
+  while(ip < in+inlen) c0[*ip++]++; 
 
   int i;
   for(i = 0; i < 256; i++) 
@@ -275,51 +273,50 @@ int hist_8_128(unsigned char *in, unsigned inlen) {
 
   int i;
   for(i = 0; i < 256; i++) 
-    c0[i] += c1[i]+c2[i]+c3[i]+c4[i]+c5[i]+c6[i]+c7[i];
+    c0[i] += c1[i]+c2[i]+c3[i]+c4[i]+c5[i]+c6[i]+c7[i];//+c8[i]+c9[i]+cA[i]+cB[i]+cC[i]+cD[i]+cE[i]+cF[i];
   RET;
 }
   #endif
 
-#define TMPRINT(__x) { printf("%s %.1f clocks/symbol \t%7.2f count=%d it=%d\n", __x, (double)cc/((double)n*it), (double)(tc>=0.000001?(((double)n*it/1048576.0)/(((double)tc/1)/TM_T)):0.0), r, it); }
+#define TMPRINT(__x) { printf("%s \t%.1f clocks/symbol \t%7.2f count=%d it=%d\n", __x, (double)cc/((double)n*it), (double)(tc>=0.000001?(((double)n*it/1048576.0)/(((double)tc/1)/TM_T)):0.0), r, it); }
 #define TMBEG for(t0=tminit(),c0=__rdtsc(),it=0;;) {
 #define TMEND it++; if((tc = tmtime() - t0) > tx) break; } cc=__rdtsc()-c0
 
-int main(int argc, char *argv[]) {
-  if(argc < 2) {
-    fprintf(stderr, "usage: %s <file>\n", argv[0]);
-    exit(-1);
+int main(int argc, char *argv[]) { int i; unsigned char *in; int n; 
+  { char *inname = argv[1];  
+
+    FILE *fi = fopen(inname,  "rb"); 
+    if(!fi ) perror(inname), exit(1);  
+    printf("'%s'\n", inname);
+
+    fseek(fi, 0, SEEK_END); 
+    long long flen = ftell(fi); 
+    fseek(fi, 0, SEEK_SET);
+
+    if(flen > BUFMAX) flen = BUFMAX;
+    n = flen; 
+    if(!(in = (unsigned char*)malloc(n))) { fprintf(stderr, "malloc error\n"); exit(-1); }
+    n = fread(in, 1, n, fi);
+    fclose(fi);
+    if(n <= 0) exit(0); 
   }
-  FILE *fi = fopen(argv[1],  "rb"); 
-  if(!fi ) perror(argv[1]), exit(1);  
-  printf("'%s'\n", argv[1]);
 
-  fseek(fi, 0, SEEK_END); 
-  long long flen = ftell(fi); 
-  fseek(fi, 0, SEEK_SET);
-
-  if(flen > (1<<30)) flen = 1<<30;
-  int n = flen; unsigned char *in = (unsigned char *)malloc(n); 
-  if(!in) { fprintf(stderr, "malloc error\n"); exit(-1); }
-  n = fread(in, 1, n, fi);
-  fclose(fi);
-  if(n <= 0) exit(0); 
-  
   { 
-    tm_t tx = 2*1000000;
+    tm_t tx=2*1000000; 
     int r,k,it;
     tm_t t0,tc,c0,cc;
-    TMBEG r = hist_4_32(  in,n);		TMEND;
+    TMBEG r = hist_4_32(  in,n);	TMEND;
       #ifdef __SSE4_1__
-    TMBEG r = hist_4_128(in,n);  		TMEND;	TMPRINT("hist_4_128");
-    TMBEG r = hist_8_128(in,n);  		TMEND;	TMPRINT("hist_8_128");
+    TMBEG r = hist_4_128(in,n);  	TMEND;	TMPRINT("hist_4_128");
+    TMBEG r = hist_8_128(in,n);  	TMEND;	TMPRINT("hist_8_128");
       #endif
-    TMBEG r = hist_4_32(  in,n);		TMEND;	TMPRINT("hist_4_32");
-    TMBEG r = hist_4_64( in,n); 		TMEND;	TMPRINT("hist_4_64");
-    TMBEG r = hist_8_64( in,n); 		TMEND;	TMPRINT("hist_8_64");
+    TMBEG r = hist_4_32( in,n);		TMEND;	TMPRINT("hist_4_32");
+    TMBEG r = hist_4_64( in,n); 	TMEND;	TMPRINT("hist_4_64");
+    TMBEG r = hist_8_64( in,n); 	TMEND;	TMPRINT("hist_8_64");
 
-    TMBEG r = hist_8_8(   in,n); 		TMEND;	TMPRINT("hist_8_8");
-    TMBEG r = hist_4_8(   in,n); 		TMEND;	TMPRINT("hist_4_8");
-    TMBEG r = hist_1_8(   in,n); 		TMEND;	TMPRINT("hist_1_8");
+    TMBEG r = hist_8_8(  in,n); 	TMEND;	TMPRINT("hist_8_8");
+    TMBEG r = hist_4_8(  in,n); 	TMEND;	TMPRINT("hist_4_8");
+    TMBEG r = hist_1_8(  in,n); 	TMEND;	TMPRINT("hist_1_8");
   }
-  free(in); 
+  free(in);
 }
